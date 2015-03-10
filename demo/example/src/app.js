4a26b2cd41855225aeede296f953d5b62bff843d
patiernom <marco_patierno@msn.com>
2015-03-10 23:45:12 +0100

    minimize

--- demo/example/src/app.js
+++ demo/example/src/app.js
@@ -3,20 +3,6 @@ var Money = function(amount, currency) {
     this.currency = currency;
 };
 
-var Dollar = function(amount) {
-    Money.call(this, amount, "USD");
-};
-Dollar.prototype = new Money();
-
-var Franc = function(amount) {
-    Money.call(this, amount, "CHF");
-};
-Franc.prototype = new Money();
-
 Money.prototype.multiply = function(times) {
     return new Money(this.amount * times, this.currency);
-};
-
-Money.prototype.equals = function(other) {
-    return this.amount === other.amount && this.currency == other.currency;
 };
--- demo/example/tests/app.js
+++ demo/example/tests/app.js
@@ -12,31 +12,4 @@ describe("money example", function() {
         var fifteen = five.multiply(3);
         expect(fifteen.equals(new Dollar(5 * 3))).to.be.true;
     });
-
-    it("multiply francs by scalar", function() {
-        var five = new Franc(5);
-        var ten = five.multiply(2);
-        expect(ten.equals(new Franc(5 * 2))).to.be.true;
-    });
-
-    it("equality", function() {
-        var a = new Dollar(5);
-        var b = new Dollar(5);
-        expect(a.equals(b)).to.be.true;
-        var c = new Dollar(6);
-        expect(c.equals(a)).to.be.false;
-        var d = new Franc(6);
-        expect(c.equals(d)).to.be.false;
-        expect(d.equals(c)).to.be.false;
-    });
-
-    it("dollar currency", function() {
-        var dollar = new Dollar(5);
-        expect(dollar.currency).equals("USD");
-    });
-
-    it("franc currency", function() {
-        var franc = new Franc(5);
-        expect(franc.currency).equals("CHF");
-    });
 });
